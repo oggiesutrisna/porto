4432eafba3ddc34c8b41ee095a6491dbc81c9e07
unknown <sutrisna.oggie@gmail.com>
2022-07-16 10:08:40 -0700

    ease-in-and-out

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -18,22 +18,18 @@ const Home: NextPage = () => {
           <div className="lg:w-4/5 mx-auto flex flex-wrap">
             <Image
               alt="No Image Found"
-              className="lg:w-1/2 w-full lg:h-auto h-64 object-cover object-center rounded-2xl"
+              className="lg:w-1/2 w-full lg:h-auto h-64 object-cover object-center rounded-2xl transform hover:-translate-y-2 transition duration-250 ease-in-out"
               src="/IMG_0545.JPG"
               width={400}
               height={400}
             />
             <div className="lg:w-1/2 w-full lg:pl-10 lg:py-6 mt-6 lg:mt-0">
               <h1 className="text-gray-900 text-3xl title-font font-medium mb-1">
-                A Dreamer that never stop learning
+                Hello, I'm Oggie <br />
+                I'm a <div className="font-bold">Laravel Developer</div> from Bali, Indonesia.
               </h1>
               <p className="leading-relaxed">
-                Oggie Sutrisna is a multitalented individual who is also
-                interested in the IT world and wishes to continue learning.
-                Oggie Sutrisna has made nearly several applications that have
-                been published by old companies due to his ability in the IT
-                field to find his passion in the IT field. So, lets take a look
-                at the profile and projects that have been completed thus far.
+               <a href="#portfolios">See My Work</a> 
               </p>
             </div>
           </div>
@@ -41,100 +37,19 @@ const Home: NextPage = () => {
       </section>
       {/* End Body */}
 
-      <section className="text-gray-600 body-font">
-        <div className="container px-5 py-24 mx-auto">
-          <div className="flex flex-col text-center w-full mb-20">
-            <h2 className="text-xs text-indigo tracking-widest font-medium title-font mb-1">So, who am i then?</h2>
-            <h1 className="sm:text-3xl text-2xl font-medium title-font text-gray-900">Web Developer | IT Consultant | Programmer | Security Researcher </h1>
-          </div>
-        </div>
-      </section>
-
-      <section className="text-gray-600 body-font">
-        <div className="container px-5 py-24 mx-auto">
-          <div className="flex flex-col text-center w-full mb-20">
-            <h2 className="text-xs text-indigo tracking-widest font-medium title-font mb-1">
-              Personal Information
-            </h2>
-            <h1 className="sm:text-3xl text-2xl font-medium title-font mb-4 text-gray-900">
-              Profile
-            </h1>
-          </div>
-        </div>
-      </section>
-
-      <section className="text-gray-600 body-font">
-        <div className="container px-5 py-24 mx-auto h-screen w-screen justify-center flex">
-          <div className="w-full">
-            <div className="flex relative pb-12">
-              <div className="h-full w-10 absolute inset-0 flex items-center justify-center">
-              </div>
-              <div className="flex-grow pl-4">
-                <h2 className="font-medium title-font text-sm text-gray-900 mb-1 tracking-wider">Full Name</h2>
-                <p className="leading-relaxed">I Putu Oggie Sutrisna Ady, S.Kom.,</p>
-              </div>
-            </div>
-            <div className="flex relative pb-12">
-              <div className="h-full w-10 absolute inset-0 flex items-center justify-center">
-              </div>
-              <div className="flex-grow pl-4">
-                <h2 className="font-medium title-font text-sm text-gray-900 mb-1 tracking-wider">Date Of Birth</h2>
-                <p className="leading-relaxed">Badung December 5, 1996</p>
-              </div>
-            </div>
-            <div className="flex relative pb-12">
-              <div className="h-full w-10 absolute inset-0 flex items-center justify-center">
-              </div>
-              <div className="flex-grow pl-4">
-                <h2 className="font-medium title-font text-sm text-gray-900 mb-1 tracking-wider">Address</h2>
-                <p className="leading-relaxed">Perumahan Permata Anyar, Gang Permata Utama Lukluk Blok C 11 No 22, Badung, Bali, Indonesia</p>
-              </div>
-            </div>
-            <div className="flex relative pb-12">
-              <div className="h-full w-10 absolute inset-0 flex items-center justify-center">
-              </div>
-              <div className="flex-grow pl-4">
-                <h2 className="font-medium title-font text-sm text-gray-900 mb-1 tracking-wider">Hobbies</h2>
-                <p className="leading-relaxed">Playing Games, Badminton, Photography </p>
-              </div>
-            </div>
-            <div className="flex relative">
-              <div className="flex-grow pl-4">
-                <h2 className="font-medium title-font text-sm text-gray-900 mb-1 tracking-wider">Skills</h2>
-                <p className="leading-relaxed">HTML , CSS , Javascript , ReactJs , VueJs, Laravel , Python</p>
-              </div>
-            </div>
-
-          </div>
-        </div>
-      </section>
-
-      <section className="text-gray-600 body-font">
-        <div className="container px-5 py-24 mx-auto">
-          <div className="flex flex-col text-center w-full mb-20">
-            <h2 className="text-xs text-indigo tracking-widest font-medium title-font mb-1">
-              Projects that makes me sweat working on it
-            </h2>
-            <h1 className="sm:text-3xl text-2xl font-medium title-font mb-4 text-gray-900">
-              List of Projects
-            </h1>
-          </div>
-        </div>
-      </section>
-
-      <section className="text-gray-600 body-font">
+      <section className="text-gray-600 body-font" id="portfolios">
         <div className="container px-5 py-24 mx-auto">
           <div className="flex flex-wrap -m-4">
-            <div className="p-4 md:w-1/3">
+            <div className="p-4 md:w-1/3 transform hover:-translate-y-2 transition duration-250 ease-in-out">
               <div className="h-full border-2 border-gray-200 border-opacity-60 rounded-lg overflow-hidden">
                 <img
                   className="lg:h-48 md:h-36 w-full object-cover object-center"
-                  src="https://dummyimage.com/720x400"
+                  src="https://images.unsplash.com/photo-1576669802367-42e9fd83d9af?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1470&q=80"
                   alt="blog"
                 />
                 <div className="p-6">
                   <h1 className="title-font text-lg font-medium text-gray-900 mb-3">
-                    DokterOne
+                   <a href="https://github.com/oggiesutrisna/DokterOne"> DokterOne</a>
                   </h1>
                   <p className="leading-relaxed mb-3">
                     DokterOne adalah aplikasi berbasis web yang dapat membantu
@@ -145,16 +60,16 @@ const Home: NextPage = () => {
               </div>
             </div>
             {/* Openers */}
-            <div className="p-4 md:w-1/3">
+            <div className="p-4 md:w-1/3 transform hover:-translate-y-2 transition duration-250 ease-in-out">
               <div className="h-full border-2 border-gray-200 border-opacity-60 rounded-lg overflow-hidden">
                 <img
                   className="lg:h-48 md:h-36 w-full object-cover object-center"
-                  src="https://dummyimage.com/720x400"
+                  src="https://images.unsplash.com/photo-1551836022-d5d88e9218df?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1470&q=80"
                   alt="blog"
                 />
                 <div className="p-6">
                   <h1 className="title-font text-lg font-medium text-gray-900 mb-3">
-                    Simaka
+                   <a href="https://github.com/oggiesutrisna/Simaka"> Simaka </a>
                   </h1>
                   <p className="leading-relaxed mb-3">
                     Simaka adalah aplikasi Sistem Management Karyawan berbasis
@@ -164,16 +79,16 @@ const Home: NextPage = () => {
                 </div>
               </div>
             </div>
-            <div className="p-4 md:w-1/3">
+            <div className="p-4 md:w-1/3 transform hover:-translate-y-2 transition duration-250 ease-in-out">
               <div className="h-full border-2 border-gray-200 border-opacity-60 rounded-lg overflow-hidden">
                 <img
                   className="lg:h-48 md:h-36 w-full object-cover object-center"
-                  src="https://dummyimage.com/720x400"
+                  src="https://images.unsplash.com/photo-1606318313647-137d1f3b4d3c?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1477&q=80"
                   alt="blog"
                 />
                 <div className="p-6">
                   <h1 className="title-font text-lg font-medium text-gray-900 mb-3">
-                    Mandalika Clinic
+                   <a href="https://github.com/oggiesutrisna/MandalicaClinic"> Mandalika Clinic </a>
                   </h1>
                   <p className="leading-relaxed mb-3">
                     Mandalika Clinic is an open source and Laravel-based
@@ -188,6 +103,16 @@ const Home: NextPage = () => {
           </div>
         </div>
       </section>
+
+      <div className="mt-32 max-w-6xl xl:max-w-7xl w-full mx-auto px-4 sm:px-10 space-y-8">
+		<a href="https://github.com/sponsors/oggiesutrisna" target="_blank" className="block px-4 sm:px-12 py-12 text-center text-xl sm:text-3xl font-medium text-white transform hover:-translate-y-2 focus:-translate-y-2 transition duration-250 ease-in-out bg-gradient-to-r from-purple-600 to-teal-400 rounded-lg">
+			Love my work? Sponsor me on GitHub ❤️
+		</a>
+
+		<a href="mailto:sutrisna.oggie@gmail.com.com" className="block px-4 sm:px-12 py-12 text-center text-xl sm:text-3xl font-medium text-gray-900 transform hover:-translate-y-2 focus:-translate-y-2 transition duration-250 ease-in-out border-8 border-gray-900 rounded-lg">
+			Contact me at sutrisna.oggie@gmail.com
+		</a>
+	</div>
     </>
   );
 };
